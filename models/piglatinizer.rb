e48ee311eb03b5489edb5f45e17d6ee05cb4c216
Reno Sorensen <renojames19@gmail.com>
2019-05-17 00:16:12 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,10 +1,14 @@
 
 class PigLatinizer
 
+  def starts_with_vowel?(text)
+    text.start_with("a", "e", "i", "o", "u")
+  end
+
   def piglatinize(text)
     text_ary = text.split("")
     text_ary = text_ary.rotate(1)
-    new_string = text_ary.join + "way"
+    new_string = text_ary.join + "ay"
     new_string
   end
 
