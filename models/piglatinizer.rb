e9cc8a8d51f981bac718070ea5825794ac46f7f7
Reno Sorensen <renojames19@gmail.com>
2019-05-17 00:37:39 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,14 +2,29 @@
 class PigLatinizer
 
   def starts_with_vowel?(text)
-    text.start_with("a", "e", "i", "o", "u")
+    text.downcase.start_with?("a", "e", "i", "o", "u")
   end
 
+  def is_uppercase?(ary)
+    ary[0].upcase == ary[0]
+  end
+
+#["H", "e", "l", "l", "o"]
+
   def piglatinize(text)
-    text_ary = text.split("")
-    text_ary = text_ary.rotate(1)
-    new_string = text_ary.join + "ay"
-    new_string
+    if starts_with_vowel?(text)
+      text_ary = text.downcase.split("")
+      text_ary = text_ary.rotate(1)
+      text_ary[1].upcase
+      new_string = text_ary.join.capitalize + "way"
+      new_string
+    else
+      text_ary = text.downcase.split("")
+      text_ary = text_ary.rotate(1)
+      new_string = text_ary.join.capitalize + "ay"
+      new_string
+    end
+
   end
 
 end
